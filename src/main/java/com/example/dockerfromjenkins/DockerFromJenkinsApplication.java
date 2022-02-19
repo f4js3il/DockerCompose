d0421f0a8d7c0ceb2dda90dcb10c8418e8c6cdf1
f4js3il <shaluchandran@gmail.com>
2022-02-19 11:14:17 -0600

    Initial Commit

---
+++ src/main/java/com/example/dockerfromjenkins/DockerFromJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerfromjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerFromJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerFromJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerfromjenkins/DockerFromJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerfromjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerFromJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
